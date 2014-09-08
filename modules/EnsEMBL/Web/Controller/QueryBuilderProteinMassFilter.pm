9458c15307ea8e853023aabbcb604bfc41e68364
Mark McDowall <mcdowall@ebi.ac.uk>
2014-09-08 11:20:54 +0100

    Modified the Protein Mass filter

--- modules/EnsEMBL/Web/Controller/QueryBuilderProteinMassFilter.pm
+++ modules/EnsEMBL/Web/Controller/QueryBuilderProteinMassFilter.pm
@@ -20,6 +20,8 @@ sub new {
   my $mass_max = $hub->param('n');
   my $callback = $hub->param('callback');
   
+  
+
   my $dbc = $hub->database('core')->dbc();
   my $sql = "select x.dbprimary_acc, tna.value from gene g, transcript ts, translation tn, translation_attrib tna, attrib_type at, xref x where ts.gene_id=g.gene_id and g.display_xref_id=x.xref_id and ts.transcript_id=tn.transcript_id and tn.translation_id=tna.translation_id and tna.attrib_type_id=at.attrib_type_id and at.code = 'MolecularWeight' and cast(tna.value AS DECIMAL) between ? and ?";
   my $sth = $dbc->prepare($sql);
