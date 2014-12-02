2ec34d5cb33c7410ca7d3807d27ed3e8d496e36a
Mark McDowall <mcdowall@ebi.ac.uk>
2014-12-02 16:20:56 +0000

    Modified the internal track description for the intron branch point data

--- modules/EnsEMBL/Web/ImageConfig/contigviewbottom.pm
+++ modules/EnsEMBL/Web/ImageConfig/contigviewbottom.pm
@@ -177,8 +177,8 @@ sub modify {
   my $pombase_menu_binding = $self->create_submenu('chromatin_binding', 'Chromatin binding');
   $gene_transcript_menu->after($pombase_menu_binding);
 
-  my $pombase_menu_binding = $self->create_submenu('intron_branch_point', 'Intron Branch Point');
-  $gene_transcript_menu->after($pombase_menu_binding);
+  my $pombase_menu_intron = $self->create_submenu('pb_intron_branch_point', 'Intron Branch Point');
+  $gene_transcript_menu->after($pombase_menu_intron);
   
   my $pombase_menu_polya   = $self->create_submenu('polya_sites', 'Polyadenylation sites');
   $gene_transcript_menu->after($pombase_menu_polya);
