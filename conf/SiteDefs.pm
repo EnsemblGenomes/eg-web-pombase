d08767994d3b0503eaccfd18b7d2f4ef249bdb76
Mark McDowall <mcdowall@ebi.ac.uk>
2015-04-01 16:47:40 +0100

    conf/SiteDefs.pm

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -14,6 +14,8 @@ sub update_conf {
     $SiteDefs::__species_aliases{ 'Schizosaccharomyces_octosporus' } = [qw(Schizosaccharomyces_octosporus)];
     
     $SiteDefs::SITE_NAME = 'PomBase';
+    $SiteDefs::GENOMIC_UNIT = 'pombase';
+    $SiteDefs::EBEYE_SEARCH_UNITS = [qw(pombase)];
     $SiteDefs::ENSEMBL_SITETYPE = 'PomBase';
     $SiteDefs::SITE_FTP= 'ftp://ftp.ensemblgenomes.org/pub/spombe';
 
