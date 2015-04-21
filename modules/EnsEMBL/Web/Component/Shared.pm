c2ddb0571269dd2643c65e488538a2b68301afe3
Mark McDowall <mcdowall@ebi.ac.uk>
2015-04-21 15:26:09 +0100

    Modified the _matches function to remove the PomBase intereaction entries

---
+++ modules/EnsEMBL/Web/Component/Shared.pm
@@ -0,0 +1,106 @@
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Component::Shared;
+
+use strict;
+
+sub _matches { ## TODO - tidy this
+  my ($self, $key, $caption, @keys) = @_;
+  my $output_as_twocol  = $keys[-1] eq 'RenderAsTwoCol';
+  my $output_as_table   = $keys[-1] eq 'RenderAsTables';
+  my $show_version      = $keys[-1] eq 'show_version' ? 'show_version' : '';
+
+  pop @keys if ($output_as_twocol || $output_as_table || $show_version) ; # if output_as_table or show_version or output_as_twocol then the last value isn't meaningful
+
+  my $object       = $self->object;
+  my $species_defs = $self->hub->species_defs;
+  my $label        = $species_defs->translate($caption);
+  my $obj          = $object->Obj;
+
+  # Check cache
+  if (!$object->__data->{'links'}) {
+    my @similarity_links = @{$object->get_similarity_hash($obj)};
+    return unless @similarity_links;
+    $self->_sort_similarity_links($output_as_table, $show_version, $keys[0], @similarity_links );
+  }
+
+  my @links = map { @{$object->__data->{'links'}{$_}||[]} } @keys;
+  return unless @links;
+  @links = $self->remove_redundant_xrefs(@links) if $keys[0] eq 'ALT_TRANS';
+  return unless @links;
+
+  my $db    = $object->get_db;
+  my $entry = lc(ref $obj);
+  $entry =~ s/bio::ensembl:://;
+
+  my @rows;
+  my $html = $species_defs->ENSEMBL_SITETYPE eq 'Vega' ? '' : "<p><strong>This $entry corresponds to the following database identifiers:</strong></p>";
+  
+  my @ex_ext_db = ('PomBase Interaction Genetic',
+                   'PomBase Interaction Physical');
+  my %exclussion_list = map { $_ => 1 } @ex_ext_db;
+
+  # in order to preserve the order, we use @links for acces to keys
+  while (scalar @links) {
+    my $key = $links[0][0];
+    my $j   = 0;
+    my $text;
+
+    # display all other vales for the same key
+    while ($j < scalar @links) {
+      my ($other_key , $other_text) = @{$links[$j]};
+      if ($key eq $other_key) {
+        $text      .= $other_text;
+        splice @links, $j, 1;
+      } else {
+        $j++;
+      }
+    }
+
+    if ( !exists($exclussion_list{$key} ) ) {
+      push @rows, { dbtype => $key, dbid => $text };
+    }
+  }
+
+  my $table;
+  @rows = sort { $a->{'dbtype'} cmp $b->{'dbtype'} } @rows;
+
+  if ($output_as_twocol) {
+    $table = $self->new_twocol;
+    $table->add_row("$_->{'dbtype'}:", " $_->{'dbid'}") for @rows;    
+  } elsif ($output_as_table) { # if flag is on, display datatable, otherwise a simple table
+    $table = $self->new_table([
+        { key => 'dbtype', align => 'left', title => 'External database' },
+        { key => 'dbid',   align => 'left', title => 'Database identifier' }
+      ], \@rows, { data_table => 'no_sort no_col_toggle', exportable => 1 }
+    );
+  } else {
+    $table = $self->dom->create_element('table', {'cellspacing' => '0', 'children' => [
+      map {'node_name' => 'tr', 'children' => [
+        {'node_name' => 'th', 'inner_HTML' => "$_->{'dbtype'}:"},
+        {'node_name' => 'td', 'inner_HTML' => " $_->{'dbid'}"  }
+      ]}, @rows
+    ]});
+  }
+
+  return $html.$table->render;
+}
+
+
+1;
