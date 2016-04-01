61cc8cac2efd672a5a5f596cc89914e32f4ec8f3
Mark McDowall <mcdowall@ebi.ac.uk>
2016-04-01 09:56:15 +0100

    Moved the TF binding sites to their own menu

---
+++ modules/EnsEMBL/Web/ImageConfig/contigviewbottom.pm
@@ -0,0 +1,119 @@
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+# $Id: contigviewbottom.pm,v 1.7 2013-11-27 14:23:52 ek3 Exp $
+
+package EnsEMBL::Web::ImageConfig::contigviewbottom;
+
+use strict;
+use warnings;
+
+use previous qw(init initialize);
+
+sub init {
+  my $self = shift;
+  
+  $self->create_menus(qw(
+    sequence
+    marker
+    trans_associated
+    transcript
+    prediction
+    dna_align_cdna
+    dna_align_est
+    dna_align_rna
+    dna_align_other
+    protein_align
+    protein_feature
+    rnaseq
+    ditag
+    simple
+    genome_attribs
+    misc_feature
+    variation
+    recombination
+    somatic
+    functional
+    multiple_align
+    conservation
+    pairwise_blastz
+    pairwise_tblat
+    pairwise_other
+    dna_align_compara
+    oligo
+    repeat
+
+    chromatin_binding
+    pb_intron_branch_point
+    polya_sites 
+    replication_profiling
+    regulatory_elements
+    tf_binding_sites
+    tss
+    transcriptome
+    nucleosome
+    dna_methylation
+    histone_mod 
+
+    wheat_alignment      
+    wheat_assembly       
+    wheat_transcriptomics
+    wheat_ests           
+    rnaseq_cultivar      
+    rnaseq_tissue        
+    resequencing  
+
+    external_data
+    user_data
+    decorations
+    information
+  ));
+
+  $self->PREV::init(@_);
+
+  $self->load_configured_bam;
+  $self->load_configured_bed;
+  $self->load_configured_bedgraph;
+  $self->load_configured_mw;
+  $self->load_configured_gff;
+
+  my $ml = $self->get_node('fg_methylation_legend');
+  $ml->remove if $ml;
+}
+
+sub initialize {
+  ## @plugin
+  ## Adds blast track to the config
+  my $self = shift;
+  $self->PREV::initialize(@_);
+
+  ## replace "BLAST/BLAST" with "BLAST"
+
+  if (my $node = $self->get_node('blast')) {
+    $node->set('caption', 'BLAST hits');
+    $node->set('name', 'BLAST hits');
+    $node->set('description', 'Track displaying BLAST hits for the selected job');
+  }
+
+  if (my $node = $self->get_node('blast_legend')) {
+    $node->set('caption', 'BLAST Legend');
+    $node->set('name', 'BLAST Legend');
+  }
+}
+
+1;
---
+++ modules/EnsEMBL/Web/ImageConfig/gene_summary.pm
@@ -0,0 +1,107 @@
+=head1 LICENSE
+
+Copyright [2009-2014] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::ImageConfig::gene_summary;
+
+use strict;
+sub init {
+  my $self = shift;
+
+  $self->set_parameters({
+    sortable_tracks => 1, # allow the user to reorder tracks
+    opt_lines       => 1, # draw registry lines
+  });
+
+## EG
+  $self->create_menus(qw(
+    sequence
+    transcript
+    rnaseq
+    prediction
+    variation
+    somatic
+    functional
+
+    chromatin_binding
+    pb_intron_branch_point
+    polya_sites 
+    replication_profiling
+    regulatory_elements
+    tf_binding_sites
+    tss
+    transcriptome
+    nucleosome
+    dna_methylation
+    histone_mod 
+
+    wheat_alignment      
+    wheat_assembly       
+    wheat_transcriptomics
+    wheat_ests           
+    rnaseq_cultivar      
+    rnaseq_tissue        
+    resequencing  
+    dna_align_cdna
+
+    external_data
+    user_data
+    other
+    information
+  ));
+##
+
+  $self->add_tracks('other',    
+    [ 'scalebar',  '', 'scalebar',  { display => 'normal', strand => 'b', name => 'Scale bar', description => 'Shows the scalebar' }],
+    [ 'ruler',     '', 'ruler',     { display => 'normal', strand => 'b', name => 'Ruler',     description => 'Shows the length of the region being displayed' }],
+    [ 'draggable', '', 'draggable', { display => 'normal', strand => 'b', menu => 'no' }],
+  );
+  
+  $self->add_tracks('information',
+    [ 'info',    '', 'text', { display => 'normal', strand => 'r', name => 'Information',            description => 'Details of the region shown in the image' }]
+  );
+  
+  $self->add_tracks('sequence',
+    [ 'contig', 'Contigs',  'contig', { display => 'normal', strand => 'r' }]
+  );
+
+  $self->load_tracks;
+  $self->load_configured_das;
+  $self->load_configured_bed;
+  $self->load_configured_bigwig;
+
+  $self->modify_configs(
+    [ 'fg_regulatory_features_funcgen', 'transcript', 'prediction', 'variation' ],
+    { display => 'off' }
+  );
+  
+
+## EG
+  $self->modify_configs(	 
+    [qw(transcript_core_ensembl transcript_core_sg transcript_core_ncRNA transcript_core_trna transcript_core_ensembl_bacteria_alignment transcript_core_ena transcript_core_ena_genes transcript_core_ncrna)],
+    {qw(display transcript_label)}
+  );
+## EG
+
+## EG
+  my $ml = $self->get_node('fg_methylation_legend');
+  $ml->remove if $ml;
+##
+
+}
+
+1;
